8a61601a88fa592d6a364d28587601295bb5da18
Samugusto <samuaugbarbosa@gmail.com>
2026-03-10 11:15:19 -0300

    atividade

---
+++ JavaScript/DOM/atividades/atividade.js
@@ -0,0 +1,63 @@
+//atividade1
+let buttn = document.querySelector("#buttn");
+buttn.addEventListener("click", function(){
+    titulo.style.color = "#0008f5";});
+
+let butto = document.querySelector("#butto");
+butto.addEventListener("click", function(){
+    titulo.style.color = "#f50000";});
+//atividade2
+let but = document.querySelector("#but");
+but.addEventListener("click", function(){
+document.getElementById("titul").classList.toggle("oculto");
+});
+
+//atividade3
+let input = document.querySelector("#texto");
+let results = document.querySelector("#results");
+input.addEventListener("keyup", function(){
+    results.textContent = input.value.length;
+});
+//atividade4
+let clicks = document.querySelector("#clicks");
+let cli = document.querySelector("#cli");
+let contador = 0;
+clicks.addEventListener("click", function(){
+    contador++;
+    cli.textContent = contador;
+});
+//atividade5/6
+let enviar = document.querySelector("#enviar");
+let inputtest = document.querySelector("#test");
+
+enviar.addEventListener("click", function(){
+    let novoitem2 = document.createElement("li");
+    novoitem2.textContent = inputtest.value;
+    
+    novoitem2.addEventListener("click", function() {
+        //extra
+        let promptresult = prompt("você tem certeza que quer exclui-lo? (s=sim n=não)")
+        if (promptresult == "s"){
+            alert("item excluido")
+        this.remove(); }if (promptresult == "n"){
+            alert("item não excluido")
+        }
+        }
+    );
+
+    document.getElementById("lista").appendChild(novoitem2);
+    inputtest.value = "";
+});
+//atividade hard
+let env = document.querySelector("#env");
+let inputnome = document.querySelector("#nome");
+let mensagem = document.querySelector("#mensagem");
+env.addEventListener("click", function(){
+if (inputnome.value == ""){
+    mensagem.style.color="#f50000";
+    mensagem.textContent="O campo está vazio!"
+}else{
+    mensagem.style.color= "#00ff37f1"
+    mensagem.textContent="Seu nome foi enviado com sucesso!"
+}
+});
