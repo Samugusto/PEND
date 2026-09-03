63259f449aed65f8dad5675ddf81b48650f5bf63
Samugusto <samuaugbarbosa@gmail.com>
2026-09-03 11:51:56 -0300

    Atividade geolocator e câmera

--- JavaScript/API/Atividade GeoCam/script.js
+++ JavaScript/API/Atividade GeoCam/script.js
@@ -1,9 +1,11 @@
 long = document.querySelector("#long");
 lati = document.querySelector("#lati");
+prec = document.querySelector("#prec");
 navigator.geolocation.getCurrentPosition(
     function (posicao) {
         lati.textContent = `Latitude: ${posicao.coords.latitude}`;
         long.textContent = `Longitude: ${posicao.coords.longitude}`;
+        prec.textContent = `Longitude: ${posicao.coords.accuracy}`;
         console.log("Latitude:", posicao.coords.latitude);
         console.log("Longitude:", posicao.coords.longitude);
         console.log("Precisão:", posicao.coords.accuracy);
